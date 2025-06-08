4d265ee6d59214ed82a8820e771ae2a4ae60889d
henia87 <134674339+henia87@users.noreply.github.com>
2025-06-08 21:58:12 +0200

    Basic setup

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testMatch: ["**/+(*.)+(spec).+(ts)"],
+  transform: {
+    "^.+\\.(ts|js|html)$": "ts-jest",
+  },
+};
