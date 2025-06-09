03f6220d7f5f4925a9ebe48fe6dc1f2b77b99d0d
henia87 <134674339+henia87@users.noreply.github.com>
2025-06-09 16:41:46 +0200

    Basic app structure

---
+++ src/app/game-engine/components/game-canvas/game-canvas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-canvas',
+  standalone: false,
+  templateUrl: './game-canvas.component.html',
+  styleUrl: './game-canvas.component.scss'
+})
+export class GameCanvasComponent {
+
+}
---
+++ src/app/game-engine/game-engine.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GameCanvasComponent } from './components/game-canvas/game-canvas.component';
+
+
+
+@NgModule({
+  declarations: [
+    GameCanvasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GameEngineModule { }
