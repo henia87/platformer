f461cfb75891d0859c76f662e5aec40e205fe0f6
henia87 <134674339+henia87@users.noreply.github.com>
2025-06-09 20:44:41 +0200

    Implement GameLoopService

--- src/app/game-engine/components/game-canvas/game-canvas.component.ts
+++ src/app/game-engine/components/game-canvas/game-canvas.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
+import { GameLoopService } from '../../../core/services/game-loop.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-game-canvas',
@@ -6,6 +8,37 @@ import { Component } from '@angular/core';
   templateUrl: './game-canvas.component.html',
   styleUrl: './game-canvas.component.scss'
 })
-export class GameCanvasComponent {
+export class GameCanvasComponent implements OnInit, OnDestroy {
+  @ViewChild('gameCanvas', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
+  private ctx!: CanvasRenderingContext2D;
+  private frameSub?: Subscription;
+  private x = 0;
 
+  private gameLoop = inject(GameLoopService);
+
+  ngOnInit(): void {
+    const canvas = this.canvasRef.nativeElement;
+    this.ctx = canvas.getContext('2d')!;
+    this.gameLoop.start();
+    this.frameSub = this.gameLoop.frame.subscribe((dt: number) => {
+      this.update(dt);
+      this.render();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.frameSub?.unsubscribe();
+    this.gameLoop.stop();
+  }
+
+  update(dt: number) {
+    this.x += 100 * dt; // Move 100px/sec
+    if (this.x > 300) this.x = 0;
+  }
+
+  render() {
+    this.ctx.clearRect(0, 0, 400, 300);
+    this.ctx.fillStyle = 'deepskyblue';
+    this.ctx.fillRect(this.x, 120, 40, 40);
+  }
 }
