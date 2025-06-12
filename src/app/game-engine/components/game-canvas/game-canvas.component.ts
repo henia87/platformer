785c5030226cb4f50f82ac08e7e54073d5e81f0a
henia87 <134674339+henia87@users.noreply.github.com>
2025-06-12 20:59:54 +0200

    Implement AssetLoaderService

--- src/app/game-engine/components/game-canvas/game-canvas.component.ts
+++ src/app/game-engine/components/game-canvas/game-canvas.component.ts
@@ -1,20 +1,30 @@
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { GameLoopService } from '../../../core/services/game-loop.service';
 import { Subscription } from 'rxjs';
+import { AssetLoaderService } from '../../../core/services/asset-loader.service';
 
 @Component({
   selector: 'app-game-canvas',
   standalone: false,
   templateUrl: './game-canvas.component.html',
-  styleUrl: './game-canvas.component.scss'
+  styleUrl: './game-canvas.component.scss',
 })
 export class GameCanvasComponent implements OnInit, OnDestroy {
-  @ViewChild('gameCanvas', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('gameCanvas', { static: true })
+  canvasRef!: ElementRef<HTMLCanvasElement>;
   private ctx!: CanvasRenderingContext2D;
   private frameSub?: Subscription;
   private x = 0;
 
   private gameLoop = inject(GameLoopService);
+  private assetLoaderService = inject(AssetLoaderService);
 
   ngOnInit(): void {
     const canvas = this.canvasRef.nativeElement;
@@ -38,7 +48,13 @@ export class GameCanvasComponent implements OnInit, OnDestroy {
 
   render() {
     this.ctx.clearRect(0, 0, 400, 300);
-    this.ctx.fillStyle = 'deepskyblue';
-    this.ctx.fillRect(this.x, 120, 40, 40);
+    // Draw player sprite if loaded, else fallback to rectangle
+    const playerImg = this.assetLoaderService.getImage('player');
+    if (playerImg) {
+      this.ctx.drawImage(playerImg, this.x, 120, 40, 40);
+    } else {
+      this.ctx.fillStyle = 'deepskyblue';
+      this.ctx.fillRect(this.x, 120, 40, 40);
+    }
   }
 }
